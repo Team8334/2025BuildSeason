58d7ec4215b3d5b2da53ac25e0efa45765e87b1c
Uxugin <git@uxugin.com>
2025-01-23 16:18:28 -0700

    fix ExampleMission

--- TitanRobotics2025/src/main/java/frc/robot/Auto/Missions/ExampleMission.java
+++ TitanRobotics2025/src/main/java/frc/robot/Auto/Missions/ExampleMission.java
@@ -2,16 +2,16 @@ package frc.robot.Auto.Missions;
 
 import frc.robot.Auto.AutoMissionChooser;
 import frc.robot.Auto.AutoMissionEndedException;
-import frc.robot.Auto.Actions.DriveForDistanceAction;
+/*import frc.robot.Auto.Actions.DriveForDistanceAction;
 import frc.robot.Auto.Actions.DriveForTimeAction;
 import frc.robot.Auto.Actions.FrontLockOnAction;
-import frc.robot.Auto.Actions.FrontPickupNoteAction;
-import frc.robot.Auto.Actions.ParallelAction;
-import frc.robot.Auto.Actions.RunScoringSystemAction;
+import frc.robot.Auto.Actions.FrontPickupNoteAction;*/
+//import frc.robot.Auto.Actions.ParallelAction;
+/*import frc.robot.Auto.Actions.RunScoringSystemAction;
 import frc.robot.Auto.Actions.ScoringSystemStateAction;
-import frc.robot.Auto.Actions.TurnDegreesAction;
+import frc.robot.Auto.Actions.TurnDegreesAction;*/
 import frc.robot.Auto.Actions.WaitAction;
-import frc.robot.Auto.Actions.SeriesAction;
+//import frc.robot.Auto.Actions.SeriesAction;
 
 public class ExampleMission extends MissionBase {
     @Override
